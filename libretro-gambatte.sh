982ae8b61f24f4fd0726ae88bf423ac5de05191b
Gerardo Zamudio <gerardozamudio@users.noreply.github.com>
2017-03-09 12:26:33 -0600

    libretro-gambatte.sh: Update build path

--- libretro-gambatte.sh
+++ libretro-gambatte.sh
@@ -58,9 +58,9 @@ curl -O https://raw.githubusercontent.com/libretro/libretro-super/master/dist/in
 
 # build and install the core
 cd $CWD
-CFLAGS="$SLKCFLAGS" CXXFLAGS="$SLKCFLAGS" make -C libgambatte/ -f Makefile.libretro
+CFLAGS="$SLKCFLAGS" CXXFLAGS="$SLKCFLAGS" make -f Makefile.libretro
 mkdir -p $PKG/usr/lib$LIBDIRSUFFIX/libretro
-cp libgambatte/${CORE}.so $PKG/usr/lib$LIBDIRSUFFIX/libretro
+cp ${CORE}.so $PKG/usr/lib$LIBDIRSUFFIX/libretro
 
 find $PKG -print0 | xargs -0 file | grep -e "executable" -e "shared object" | grep ELF \
   | cut -f 1 -d : | xargs strip --strip-unneeded 2> /dev/null || true
