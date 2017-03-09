f4c601ad874e738a438db2b685c1949ee6310ef1
Gerardo Zamudio <github@slack.mx>
2017-03-09 12:29:12 -0600

    libretro-imame4all.sh: Rename to libretro-mame2000.sh

--- libretro-mame2000.sh
+++ libretro-mame2000.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-REPO=imame4all-libretro
-CORE=imame4all_libretro
-PRGNAM=libretro-imame4all
+REPO=mame2000-libretro
+CORE=mame2000_libretro
+PRGNAM=libretro-mame2000
 TMP=${TMP:-/tmp}
 PKG=$TMP/package-$REPO
 BUILD=1dc
@@ -58,9 +58,9 @@ curl -O https://raw.githubusercontent.com/libretro/libretro-super/master/dist/in
 
 # build and install the core
 cd $CWD
-CFLAGS="$SLKCFLAGS" CXXFLAGS="$SLKCFLAGS" make -f makefile.libretro
+CFLAGS="$SLKCFLAGS" CXXFLAGS="$SLKCFLAGS" make
 mkdir -p $PKG/usr/lib$LIBDIRSUFFIX/libretro
-cp libretro.so $PKG/usr/lib$LIBDIRSUFFIX/libretro/$CORE.so
+cp ${CORE}.so $PKG/usr/lib$LIBDIRSUFFIX/libretro/$CORE.so
 
 find $PKG -print0 | xargs -0 file | grep -e "executable" -e "shared object" | grep ELF \
   | cut -f 1 -d : | xargs strip --strip-unneeded 2> /dev/null || true
